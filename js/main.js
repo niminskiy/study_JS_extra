a57a8b3d2803cbef4f2d09066ea42e7f5261a01e
niminskiy <s.niminskiy@gmail.com>
2020-06-10 01:54:39 +0300

    Update main.js
    Готово 2 из 3, есть проблема в 2

---
+++ js/main.js
@@ -0,0 +1,31 @@
+let lang = 'ru';
+
+if (lang == 'ru'){
+	let arr = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+	console.log(arr);
+} else if (lang == 'en'){
+	let arr = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'];
+	console.log(arr);
+}
+
+
+// switch (lang){
+// 	case 'ru':
+// 		var arr = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+// 		break;
+// 	case 'en':
+// 		var arr = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'];
+// 		break;
+// }
+// console.log(arr);
+
+
+// let arr = {
+// 	'ru':['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'],
+// 	'en':['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'],
+// }
+// console.log(arr[lang]);
+
+
+
+
