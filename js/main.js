0fd8ef545358de8de45e45aabacf536a95b89bec
niminskiy <s.niminskiy@gmail.com>
2020-06-10 10:37:24 +0300

    Update main.js
    Done!

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
-let lang = 'ru';
+
+
+let lang = 'en';
+let arr;
 
 if (lang == 'ru'){
 	let arr = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
@@ -8,23 +11,29 @@ if (lang == 'ru'){
 	console.log(arr);
 }
 
+switch (lang){
+	case 'ru':
+		arr = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+		break;
+	case 'en':
+		arr = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'];
+		break;
+}
+console.log(arr);
 
-// switch (lang){
-// 	case 'ru':
-// 		var arr = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
-// 		break;
-// 	case 'en':
-// 		var arr = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'];
-// 		break;
-// }
-// console.log(arr);
+arr = {
+	'ru':['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'],
+	'en':['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'],
+}
+console.log(arr[lang]);
 
 
-// let arr = {
-// 	'ru':['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'],
-// 	'en':['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'],
-// }
-// console.log(arr[lang]);
+let namePerson = 'Артем';
+
+let result = (namePerson === 'Артем') ? 'директор' : (namePerson === 'Максим') ? 'преподаватель' : 'студент';
+console.log(result);
+
+
 
 
 
