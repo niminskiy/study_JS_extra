8cb64b47e5f7520bc3878bc8715302a31e120181
niminskiy <s.niminskiy@gmail.com>
2020-06-13 09:49:24 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,6 +6,59 @@
 
 // — Если строка более 30 знаков - то после 30го символа часть текста скрывается и вместо них появляются три точки (...)
 
+
+
+
+// function test(str) {
+// 	if (typeof str !== 'string') {
+// 		alert('аргумент не строка');
+// 		return;
+// 	}
+
+// 	str = str.trim(); 
+// 	return str.length > 50 ? str.slice(0, 50) + '...' : str;
+// }
+
+
+
+
 'use strict';
+
+
+// function testString(x){
+// 	if(typeof str !== 'string') {
+// 		console.log(str + ' - аргумент не строка!!!');
+// 		return;
+// 	}
+	
+// 	let str = x.trim();
+// 	console.log(x);
+
+// 	//str = str.lenght > 5 ? str.slice(0, 5) + '...' : str;
+
+// 	let cut = str.lenght;
+// 	console.log(cut);
+// 	str = str.slice(0, 5);
+// }
+// testString('sdfsdfgsdf');
+// //testString(123123123);
+
+
+
+
+
+
+// строка должна передаваться как параметр, а не присваиваться, но я это задание не смогу принять пока не приняты основные до текущего дня. я об этом уже говорил, это правило академии направленное на то, чтобы не допускать множества долгов
+
+
+
+function one() {
+	let x = 10;
+
+	function two(y){
+		return x + y;
+	}
+	return two(15);
+}
 
 
